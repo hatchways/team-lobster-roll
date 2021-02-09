e75e519c5237a2da69ce00a24dfc0464081648fd
LinXia <linyianxia@gmail.com>
2021-02-09 15:46:16 -0500

    add: multiple columns functionality, adjust column style

--- client/src/pages/List.js
+++ client/src/pages/List.js
@@ -14,12 +14,19 @@ const useStyles = makeStyles({
     paddingBottom: "5rem",
     backgroundColor: "#FFFFFF",
   },
+  columnsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   column: {
-    margin: 0,
+    margin: "1rem",
     padding: 0,
     backgroundColor: "#F4F6FF",
     borderRadius: "8px",
     width: "320px",
+    minHeight: "500px",
   },
   card: {
     listStyle: "none",
@@ -33,53 +40,109 @@ const useStyles = makeStyles({
   placeholder: {
     listStyle: "none",
   },
+  cardHeader: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
+
+const columnTypes = {
+  ToDo: {
+    name: "To do",
+    items: ["math", "english", "science", "history"],
+  },
+  InProgress: {
+    name: "In progress",
+    items: ["art", "philosophy", "cooking", "gym"],
+  },
+};
 
 function List() {
   const classes = useStyles();
-  const examples = ["math", "english", "science", "history"];
-  const [list, setList] = useState(examples);
+  const [columns, setColumns] = useState(columnTypes);
 
-  function handleOnDragEnd(res) {
-    if (!res.destination) return;
-    const items = Array.from(list);
-    const [reorderedItem] = items.splice(res.source.index, 1);
-    items.splice(res.destination.index, 0, reorderedItem);
-    setList(items);
+  function handleOnDragEnd(result, columns, setColumns) {
+    if (!result.destination) return;
+    const { source, destination } = result;
+
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId];
+      const destinationColumn = columns[destination.droppableId];
+      const sourceItems = [...sourceColumn.items];
+      const destinationItems = [...destinationColumn.items];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destinationItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems,
+        },
+        [destination.droppableId]: {
+          ...destinationColumn,
+          items: destinationItems,
+        },
+      });
+    } else {
+      const column = columns[source.droppableId];
+      const copiedItems = [...column.items];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          items: copiedItems,
+        },
+      });
+    }
   }
 
   return (
     <div className={classes.mainContainer}>
       <Typography variant="h1">List</Typography>
-      <DragDropContext onDragEnd={handleOnDragEnd}>
-        <Droppable droppableId="column">
-          {(provided) => (
-            <Grid
-              className={classes.column}
-              container
-              direction="column"
-              justify="center"
-              {...provided.droppableProps}
-              ref={provided.innerRef}>
-              {list.map((e, idx) => (
-                <Draggable key={e} draggableId={e} index={idx}>
+      <DragDropContext
+        onDragEnd={(result) => handleOnDragEnd(result, columns, setColumns)}>
+        <div className={classes.columnsContainer}>
+          {Object.entries(columns).map(([id, column]) => {
+            return (
+              <div>
+                <Typography variant="h3" className={classes.cardHeader}>
+                  {column.name}
+                </Typography>
+                <Droppable droppableId={id}>
                   {(provided) => (
-                    <Card
-                      className={classes.card}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
+                    <Grid
+                      className={classes.column}
+                      container
+                      direction="column"
+                      justify="flex-start"
+                      {...provided.droppableProps}
                       ref={provided.innerRef}>
-                      {e}
-                    </Card>
+                      {column.items.map((e, idx) => (
+                        <Draggable key={e} draggableId={e} index={idx}>
+                          {(provided) => (
+                            <Card
+                              className={classes.card}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              ref={provided.innerRef}>
+                              {e}
+                            </Card>
+                          )}
+                        </Draggable>
+                      ))}
+                      <span className={classes.placeholder}>
+                        {provided.placeholder}
+                      </span>
+                    </Grid>
                   )}
-                </Draggable>
-              ))}
-              <span className={classes.placeholder}>
-                {provided.placeholder}
-              </span>
-            </Grid>
-          )}
-        </Droppable>
+                </Droppable>
+              </div>
+            );
+          })}
+        </div>
       </DragDropContext>
     </div>
   );
