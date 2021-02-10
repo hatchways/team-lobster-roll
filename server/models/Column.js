c65bc3462c19489442053d823d0a750718d4830a
shinjam3 <james-shin-1998@hotmail.com>
2021-02-10 17:37:35 -0500

    adjusted schema statics and methods as well as added new ones

--- server/models/Column.js
+++ server/models/Column.js
@@ -1,38 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Card = require('./Card');
+const Card = require("./Card");
 
-const ColumnSchema = new Schema({
-	name: {
-		type: String,
-		required: true
-	},
-	cards: [Card.schema]
-}, {collection: 'Column'});
+const ColumnSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    cards: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Card",
+      },
+    ],
+  },
+  { collection: "Column" }
+);
 
 // creates a new instance of the Column model and returns the saved instance
-ColumnSchema.statics.createNewColumn = async function(name) {
-	const column = new this({ name });
-	await column.save();
-	return column;
+ColumnSchema.statics.createNewColumn = async function (name) {
+  const column = new this({ name });
+  await column.save();
+  return column;
 };
 
 // deletes a Column in the db by its _id
-ColumnSchema.statics.deleteColumn = function(columnId) {
-	this.deleteOne({ _id: mongoose.Types.ObjectId(columnId)}).exec();
+ColumnSchema.statics.deleteColumn = async function (columnId) {
+  await this.deleteOne({ _id: mongoose.Types.ObjectId(columnId) });
 };
 
 // adds a Card model into the Column's cards array
-ColumnSchema.methods.addNewCard = function(card) {
-	this.cards.push(card);
-	await this.save();
+ColumnSchema.methods.addCard = async function (cardId) {
+  this.cards.push(cardId);
+  await this.save();
 };
 
 // removes a Card model by _id from the Column's cards array
-ColumnSchema.methods.removeCard = function(cardId) {
-	this.cards.pull({ _id: cardId });
-	await this.save();
+ColumnSchema.methods.removeCard = async function (cardId) {
+  this.cards.pull({ _id: mongoose.Types.ObjectId(cardId) });
+  await Promise.all([this.save(), Card.deleteCard(cardId)]);
 };
 
-const Column = mongoose.model('Column', ColumnSchema);
+const Column = mongoose.model("Column", ColumnSchema);
 module.exports = Column;
