ded90fa049496a98d08508586bd47d60e5f26800
moods-72 <d_healthy1@yahoo.ca>
2021-02-14 12:05:07 -0500

    Functionality added to login and signup.

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,54 +1,56 @@
 import React from "react";
-import {Link} from 'react-router-dom';
-import {makeStyles} from '@material-ui/core';
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
-  '@keyframes titleMove':{
+  "@keyframes titleMove": {
     from: {
-      transform: "translate(50px,-20px)"
-    }
+      transform: "translate(50px,-20px)",
+    },
   },
-  '@keyframes sloganMove':{
+  "@keyframes sloganMove": {
     from: {
-      transform: "translate(-50px, 20px)"
-    }
+      transform: "translate(-50px, 20px)",
+    },
   },
-  main:{
+  main: {
     width: "100%",
     height: "100vh",
     fontFamily: "Roboto",
     color: "rgb(41, 140, 238)",
     display: "grid",
-    placeItems:"center",
-    textAlign:"center"
+    placeItems: "center",
+    textAlign: "center",
   },
-  title:{
+  title: {
     fontSize: "72px",
     marginBottom: "-30px",
-    animation: "$titleMove 2s ease"
-    
+    animation: "$titleMove 2s ease",
   },
-  slogan:{
-    animation: "$sloganMove 2s ease"
+  slogan: {
+    animation: "$sloganMove 2s ease",
   },
-  link:{
+  link: {
     marginRight: "20px",
     textDecoration: "none",
-    color: "rgb(41, 140, 238)"
-  }
-})
+    color: "rgb(41, 140, 238)",
+  },
+});
 
-function LandingPage (){
+function LandingPage() {
   const classes = useStyles();
   return (
     <div className={classes.main}>
       <div>
         <h1 className={classes.title}>Kanban</h1>
         <h3 className={classes.slogan}>Your school life organized.</h3>
-        <Link to="/login" className={classes.link}>Login</Link>
-        <Link to="/signup" className={classes.link}>Sign up</Link>
+        <Link to="/login" className={classes.link}>
+          Login
+        </Link>
+        <Link to="/signup" className={classes.link}>
+          Sign up
+        </Link>
       </div>
-      
     </div>
   );
 }
