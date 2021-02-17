f2c386f81b24f7f84ae326d46426b72dc48f3f1e
shinjam3 <james-shin-1998@hotmail.com>
2021-02-17 16:41:09 -0500

    Created detailed card component

---
+++ client/src/pages/DetailedCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  
+});
+
+function CardInfo() {
+  return (
+    <React.Fragment>
+			<Container>
+		</React.Fragment>
+  );
+}
+
+export default CardInfo;
