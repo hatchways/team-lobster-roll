401b95c3307ccb6782ead7bb8137846238aaaf6d
LinXia <linyianxia@gmail.com>
2021-02-18 22:24:53 -0500

    add: API calls to create Board/Col/Card via UI

--- client/src/API/card.js
+++ client/src/API/card.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const createBoard = (data) => {
+export const createCard = (data) => {
   try {
-    const URL = `${window.location.origin}/api/board/`;
+    const URL = `${window.location.origin}/api/card/`;
     axios.post(URL, data);
   } catch (err) {
     console.error(err);
--- client/src/API/column.js
+++ client/src/API/column.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const createBoard = (data) => {
+export const createColumn = (data) => {
   try {
-    const URL = `${window.location.origin}/api/board/`;
+    const URL = `${window.location.origin}/api/column/`;
     axios.post(URL, data);
   } catch (err) {
     console.error(err);
