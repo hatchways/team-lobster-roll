fc7256544966832501de431d0469ac580aa4f209
moods-72 <d_healthy1@yahoo.ca>
2021-02-19 11:37:26 -0500

    Refactored to remove deprecation warnings.

--- client/src/pages/Upload.jsx
+++ client/src/pages/Upload.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import axios from "axios";
-import ImageUploader from "react-images-upload";
 import { makeStyles, Button } from "@material-ui/core";
 import HashLoader from "react-spinners/HashLoader";
 
@@ -16,6 +15,9 @@ const useStyles = makeStyles({
     fontSize: "36px",
     color: "#759CFC",
   },
+  input: {
+    display: "none",
+  },
   button: {
     color: "white",
     marginTop: 30,
@@ -23,55 +25,62 @@ const useStyles = makeStyles({
 });
 
 function Upload() {
+  const hiddenFileInput = useRef(null);
   const classes = useStyles();
   const [pictures, setPictures] = useState([]);
   const [imageName, setImageName] = useState("");
   const [loading, setLoading] = useState(false);
 
   const uploadImage = () => {
-    console.log(pictures)
     setLoading(true);
     let uploadPromises = pictures.map((picture) => {
       let data = new FormData();
       data.append("image", picture, picture.name);
       setPictures([]);
-      //return axios.post(`${window.location.origin}/upload/`, data);
+      return axios.post(`${window.location.origin}/upload/`, data);
     });
-    /*
+
     axios
       .all(uploadPromises)
       .then((data) => {
         setLoading(false);
-        setImageName("");
+        setImageName("Uploaded completed.");
         console.log(data[0].data);
       })
       .catch((err) => console.log(err));
-    setPictures([]);*/
+    setPictures([]);
   };
 
-  const onDrop = (picture) => {
-    setImageName(picture[0].name);
-    setPictures(pictures.concat(picture));
+  const onDrop = (e) => {
+    setImageName(e.target.files[0].name);
+    setPictures(pictures.concat(e.target.files[0]));
   };
+  const chooseFile = e =>{
+    hiddenFileInput.current.click();
+  }
 
   return (
     <div className={classes.main}>
       <h1 className={classes.title}>Image Uploader</h1>
       {!loading && (
-        <ImageUploader
-          withIcon={true}
-          withPreview={false}
-          buttonText="Choose image"
-          buttonStyles={{
-            background: "#759CFC",
-            borderRadius: "5px",
-            height: "35px",
-          }}
-          onChange={onDrop}
-          imgExtension={[".jpg", ".gif", ".png"]}
-          maxFileSize={5242880}
-          singleImage={false}
-        />
+        <div>
+          <Button
+            className={classes.button}
+            variant="contained"
+            color="primary"
+            onClick={(e)=> chooseFile(e)}
+          >
+            Choose a file
+          </Button>
+          <input
+            ref={hiddenFileInput}
+            type="file"
+            className={classes.input}
+            multiple={true}
+            accept=".json,.csv,.txt,.png,.text,application/json,text/csv,text/plain"
+            onChange={(e) => onDrop(e)}
+          />
+        </div>
       )}
       {loading && <HashLoader color={"#759CFC"} />}
       <p>{imageName}</p>
