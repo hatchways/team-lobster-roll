daa2708afa2d2bcfb53f4942fdfe445f1d221fd9
LinXia <linyianxia@gmail.com>
2021-02-22 21:39:40 -0500

    add: GET API getBoard()

--- client/src/API/board.js
+++ client/src/API/board.js
@@ -8,3 +8,12 @@ export const createBoard = (data) => {
     console.error(err);
   }
 };
+
+export const getBoard = async (boardId) => {
+  try {
+    const URL = `${window.location.origin}/api/board/${boardId}`;
+    return await axios.get(URL);
+  } catch (err) {
+    console.error(err);
+  }
+};
