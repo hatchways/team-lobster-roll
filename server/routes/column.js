3e33c0fa0d82c0a26e79db5aa414761c35fb2785
shinjam3 <james-shin-1998@hotmail.com>
2021-02-24 23:42:44 -0500

    integrated delete column

--- server/routes/column.js
+++ server/routes/column.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
 const Board = require("../models/Board");
 const Column = require("../models/Column");
 const Card = require("../models/Card");
@@ -20,6 +21,18 @@ router.post("/", async (req, res, next) => {
   } catch (err) {
     console.error(err);
   }
+});
+
+// DELETE
+router.delete('/delete', async (req, res) => {
+	try {
+		const foundBoard = await Board.findBoard(req.body.boardId);
+		await foundBoard.removeColumn(req.body.columnId);
+		res.sendStatus(200);
+	} catch (err) {
+		res.status(400).send({msg: 'An error occured, could not delete column.'});
+		console.error(err);
+	}
 });
 
 module.exports = router;
